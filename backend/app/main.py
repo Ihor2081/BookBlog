657f97e4b6d337945595f987ca94369cad3b5de7
@Ihor2081 <ihor.yaremenko8@gmail.com>
2026-05-24 17:11:48 +0300

    Add allowed origin https://book-blog-omega.vercel.app

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://book-blog-omega.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
