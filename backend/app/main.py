fb554cd7ae21bb34491c6834a6be1da512ff6414
@Ihor2081 <ihor.yaremenko8@gmail.com>
2026-05-26 14:58:50 +0300

    fix: cors issue in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,7 @@ app.router.redirect_slashes = False
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://book-blog-omega.vercel.app"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://book-blog-omega.vercel.app", "https://book-blog-omega.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
