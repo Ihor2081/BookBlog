5ce046c678af962a3722fa0702e582a258e85f9f
@Ihor2081 <ihor.yaremenko8@gmail.com>
2026-05-26 17:16:27 +0300

    fix allow headers

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,7 @@ app.add_middleware(
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"], # Явно перелічи методи
-    allow_headers=["Content-Type", "Authorization", "Access-Control-Allow-Origin"],
+    allow_headers=["*"],
 )
 
 # 2. Підключення маршрутизаторів
